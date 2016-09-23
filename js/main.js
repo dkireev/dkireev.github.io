2d4fd5d97cb1a2c81e17ff40bcba51ef6c6600a4
Dmitry <dkireev.com.ua@gmail.com>
2016-09-23 19:23:15 +0300

    as

--- js/main.js
+++ js/main.js
@@ -1,84 +1,19 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        singleItem: true,
-        slideSpeed: 500,
-        pagination: true,
-        responsive: true,
-        touchDrag: true
-    });
+    $('.main').animate({ opacity: "1" }, 1000);
 
-    $('td:nth-child(2)').hover(function() {
-        $('td:nth-child(2)').css('background-color', '#fef6f1')
+    $('.buttonRecent').hover(function() {
+        $('.buttonRecent').css('background-color', 'lightgrey');
     }, function() {
-        $('td:nth-child(2)').css('background-color', '#ffffff')
+        $('.buttonRecent').css('background-color', 'white');
     });
-    $('td:nth-child(3)').hover(function() {
-        $('td:nth-child(3)').css('background-color', '#f1f9f5')
+    $('.buttonContact').hover(function() {
+        $('.buttonContact').css('background-color', 'lightgrey');
     }, function() {
-        $('td:nth-child(3)').css('background-color', '#ffffff')
-    });
-    $('td:nth-child(4)').hover(function() {
-        $('td:nth-child(4)').css('background-color', '#fbe8e8')
-    }, function() {
-        $('td:nth-child(4)').css('background-color', '#ffffff')
-    });
-    $('td:last-child').hover(function() {
-        $('td:last-child').css('background-color', '#fbf6fa')
-    }, function() {
-        $('td:last-child').css('background-color', '#ffffff')
+        $('.buttonContact').css('background-color', 'white');
     });
 
-    var screenHeight = $(window).height();
-    var varHeight;
-    if (screenHeight >= 900) {
-        varHeight = screenHeight;
-    } else {
-        varHeight = screenHeight*screenHeight/900;
-    }
-    var headerHeight = $('.header').height();
-    var carouselHeight = screenHeight - headerHeight;
-    console.log(screenHeight);
-
-    $('.section1_cam').height(carouselHeight+'px');
-    $('.section1_view').height(carouselHeight+'px');
-    $('.section1_power').height(carouselHeight+'px');
-    $('.section1_style').height(carouselHeight+'px');
-    $('.text_sec1').css('padding-top',varHeight*0.3+'px');
-    $('.section1_camImg').height(carouselHeight*0.793+'px');
-    $('.section1_viewImg').height(carouselHeight*0.767+'px');
-    $('.section1_powerImg').height(carouselHeight*0.8+'px');
-    $('.section1_styleImg').height(carouselHeight*0.779+'px');
-
-    $('.section2').height(screenHeight+'px');
-    $('.sectionLogo2').css('top',screenHeight*0.072+'px');
-    $('.text_sec2').css('padding-top',varHeight*0.23+'px');
-    $('.sectionDouble').css('bottom',varHeight*0.015+'px');
-    $('#doubleCamera').height(screenHeight*0.894+'px');
-    $('.sectionAngle').css('bottom',varHeight*0.1+'px');
-    $('#angle120').height(varHeight*0.24+'px');
-
-    $('.section3').height(screenHeight+'px');
-    $('.sectionLogo3').css('top',screenHeight*0.072+'px');
-    $('.text_sec3').css('padding-top',varHeight*0.23+'px');
-    $('#viewMain').height(screenHeight*0.672+'px');
-    $('.section3Drawing').css('bottom',varHeight*0.153+'px');
-    $('#viewDrawing').height(varHeight*0.12+'px');
-
-    $('.section4').height(screenHeight+'px');
-    $('.sectionLogo4').css('top',screenHeight*0.072+'px');
-    $('.text_sec4').css('padding-top',varHeight*0.23+'px');
-    $('#powerMain').height(screenHeight*0.817+'px');
-    $('.section4Drawing').css('bottom',varHeight*0.173+'px');
-    $('#powerDrawing').height(varHeight*0.128+'px');
-    $('.section4Hour').css('bottom',varHeight*0.402+'px');
-
-    $('.section5').height(screenHeight+'px');
-    $('.sectionLogo5').css('top',screenHeight*0.072+'px');
-    $('.text_sec5').css('padding-top',varHeight*0.23+'px');
-    $('#styleMain').height(screenHeight*0.817+'px');
-    $('.section5Drawing').css('bottom',varHeight*0.147+'px');
-    $('#styleDrawing').height(varHeight*0.169+'px');
-
-    $('.section6').height(screenHeight+'px');
+    $('.buttonContact').click(function() {
+        $('.contactFrom').animate({ opacity: "1" }, 1000);
+        $('.sections').animate({ opacity: "0" }, 500);
+    });
 });
---
+++ js/send.php
@@ -0,0 +1,43 @@
+<?php
+  //If a form has been submited
+  if(isset($_POST['submit'])) {
+ //Проверка Поля ИМЯ
+  if(trim($_POST['firstname']) == '') {
+  $hasError = true;
+  } else {
+  $name = trim($_POST['firstname']);
+  }
+ //Validate fill-in message field
+  if(trim($_POST['subject']) == 'question about') {
+  $hasError = true;
+  } else {
+  $subject = trim($_POST['subject']);
+  }
+ //Validate correct e-mail
+  if(trim($_POST['email']) == '')  {
+  $hasError = true;
+  } else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['email']))) {
+  $hasError = true;
+  } else {
+  $email = trim($_POST['email']);
+  }
+ //Validate fill-in message field
+  if(trim($_POST['message']) == '') {
+  $hasError = true;
+  } else {
+  if(function_exists('stripslashes')) {
+  $comments = stripslashes(trim($_POST['message']));
+  } else {
+  $comments = trim($_POST['message']);
+  }
+  }
+ //Если ошибок нет, отправить email
+  if(!isset($hasError)) {
+  $emailTo = 'dkireev.com.ua@gmail.com'; //Your e-mail
+  $body = "Name: $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
+  $headers = 'From: My Site <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+ mail($emailTo, $subject, $body, $headers);
+  $emailSent = true;
+  }
+  }
+  ?>
