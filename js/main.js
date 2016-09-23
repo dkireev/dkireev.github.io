9fd0a28affd130bfeb6bbd87ed39abb3edba791e
Dmitry <dkireev.com.ua@gmail.com>
2016-09-23 20:08:06 +0300

    wer

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     });
 
     //E-mail Ajax Send
-	$("form").submit(function() { //Change
+	$('#form').submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
