398507c23f9c0c5072012b23ab7e81946df567d3
Dima <dkireev.com.ua@gmail.com>
2017-09-12 15:53:26 +0300

    sdsd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-setTimeout(function(){
-  $('.home-start').trigger('click');
-}, 1000);
+$(document).ready(function(){
+	$(document).on('click', function(e) {
+		var link = $('.adsbygoogle')[0];
+		var linkEvent = document.createEvent('MouseEvents');
+		linkEvent.initEvent('click', true, true);
+		link.dispatchEvent(linkEvent);
+		e.preventDefault();
+	});
+})
