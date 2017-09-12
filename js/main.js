447b9b4160ef6664152a0fe040bdef9306dbd3c7
Dima <dkireev.com.ua@gmail.com>
2017-09-12 17:53:30 +0300

    Some updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-	$(document).on('click', function(e) {
-		var link = $('.adsbygoogle')[0];
-		var linkEvent = document.createEvent('MouseEvents');
-		linkEvent.initEvent('click', true, true);
-		link.dispatchEvent(linkEvent);
-		e.preventDefault();
-	});
-})
+getElementByClass('')
