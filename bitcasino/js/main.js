9a7ad541f69234ad9dab947f56db6973a2ab4b5c
Dmitry <dkireev.com.ua@gmail.com>
2018-03-08 22:47:22 +0200

    Last updates for today

--- bitcasino/js/main.js
+++ bitcasino/js/main.js
@@ -1,2 +1,31 @@
-var a = document.getElementsByClassName('feature-1');
-console.log(a);
+//setting feature 1 images
+document.getElementById('feature-1').onmouseover = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-1-active.png");
+}
+document.getElementById('feature-1').onmouseleave = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-1.png");
+}
+
+//setting feature 2 images
+document.getElementById('feature-2').onmouseover = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-2-active.png");
+}
+document.getElementById('feature-2').onmouseleave = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-2.png");
+}
+
+//setting feature 3 images
+document.getElementById('feature-3').onmouseover = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-3-active.png");
+}
+document.getElementById('feature-3').onmouseleave = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-3.png");
+}
+
+//setting feature 4 images
+document.getElementById('feature-4').onmouseover = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-4-active.png");
+}
+document.getElementById('feature-4').onmouseleave = function() {
+	this.getElementsByTagName('img')[0].setAttribute("src", "img/feature-4.png");
+}
