f66c86a0dd565c42696b4c69e1ec5cc40ffa59b9
Dima Kireiev <dkireev.com.ua@gmail.com>
2018-11-08 12:58:47 +0200

    Updated code

--- deposit-vova/script.js
+++ deposit-vova/script.js
@@ -1,83 +1,95 @@
-function Counter(myObj) {
-  const nextGoal = myObj.nextGoal;
-  const millisecondsInMonth = 60 * 60 * 24 * 30.4375 * 1000;
-  const secondsInMonth = millisecondsInMonth / 1000;
+counter();
 
-  const startDate = Date.parse(myObj.startDate);
-  const startAmount = myObj.startAmount;
-  const interestInMonth = myObj.interestInMonth;
-
-  let currentAmount = 0;
-  let cashflow = 0;
-  let currentTime = 0;
-  let capital = 0;
-  let goalCompletion = 0;
-  let capitalInteger = 0;
-  let capitalDecimal = 0;
-  let cashflowInteger = 0;
-  let cashflowDecimal = 0;
-  let capitalGrow = 0;
-  let goalCompletionInteger = 0;
-  let goalCompletionDecimal = 0;
-
-  function SpaceAdder(a) {
-    return a.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, "$1 ");
-  }
-
-  function DecimalExtractor(b) {
-    const a = Math.floor(b % 1 * 100);
-    const length = (a).toString().length;
-    if (length == 2) {
-      return a;
+function updateGoalFunction(value) {
+    if (value) {
+        localStorage.setItem("nextGoal", value);
+        document.getElementById("popup").style.left = "100vw";
+        setTimeout(function() { location.reload(); }, 500);
     } else {
-      return ('0' + a);
+        console.log("Wrong value");
     }
-  }
-
-  setInterval(function() {
-    currentTime = Date.now();
-
-    currentAmount = startAmount + startAmount * interestInMonth /
-      millisecondsInMonth * (currentTime - startDate);
-    cashflow = (currentAmount * interestInMonth).toFixed(2);
-
-    capital = currentAmount.toFixed(2);
-    capitalInteger = Math.floor(capital);
-    capitalDecimal = DecimalExtractor(capital);
-    document.getElementById("capitalInteger").innerHTML = SpaceAdder(
-      capitalInteger
-    );
-    document.getElementById("capitalDecimal").innerHTML =
-      ". " + capitalDecimal + " &#8372;";
-
-    cashflowInteger = Math.floor(cashflow);
-    cashflowDecimal = DecimalExtractor(cashflow);
-    document.getElementById("cashflowInteger").innerHTML = SpaceAdder(
-      cashflowInteger
-    );
-    document.getElementById("cashflowDecimal").innerHTML =
-      ". " + cashflowDecimal + " &#8372;";
-
-    goalCompletion = ((parseInt(capital) * 100) / nextGoal).toFixed(2);
-    goalCompletionInteger = Math.floor(goalCompletion);
-    goalCompletionDecimal = DecimalExtractor(goalCompletion);
-    document.getElementById("completionInteger").innerHTML = SpaceAdder(
-      goalCompletionInteger
-    );
-    document.getElementById("completionDecimal").innerHTML =
-      ". " + goalCompletionDecimal + " %";
-
-    capitalGrow = Math.round(secondsInMonth / Math.floor(cashflow));
-    document.getElementById("grow").innerHTML = SpaceAdder(Math.round(capitalGrow / 60));
-  }, 1000);
 }
 
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    const myObj = JSON.parse(this.responseText);
-    Counter(myObj);
-  }
-};
-xmlhttp.open("GET", "numbers.txt", true);
-xmlhttp.send();
+function openPopup() {
+    document.getElementById("popup").style.left = 0;
+}
+
+function counter(myObj) {
+    localStorage.setItem("startDate", "Oct 27, 2018");
+    localStorage.setItem("startAmount", "79864.78");
+    localStorage.setItem("interestInMonth", "0.0107");
+
+    const nextGoal = parseInt(localStorage.getItem("nextGoal"));
+    const millisecondsInMonth = 60 * 60 * 24 * 30.4375 * 1000;
+    const secondsInMonth = millisecondsInMonth / 1000;
+
+    const startDate = Date.parse(localStorage.getItem("startDate"));
+    const startAmount = parseFloat(localStorage.getItem("startAmount"));
+    const interestInMonth =
+        parseFloat(localStorage.getItem("interestInMonth"));
+
+    let currentAmount = 0;
+    let currentTime = 0;
+    let capital = 0;
+    let cashflow = 0;
+    let goalCompletion = 0;
+    let capitalInteger = 0;
+    let capitalDecimal = 0;
+    let cashflowInteger = 0;
+    let cashflowDecimal = 0;
+    let capitalGrow = 0;
+    let goalCompletionInteger = 0;
+    let goalCompletionDecimal = 0;
+
+    function SpaceAdder(a) {
+        return a.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, "$1 ");
+    }
+
+    function DecimalExtractor(b) {
+        const a = Math.floor(b % 1 * 100);
+        const length = (a).toString().length;
+        if (length == 2) {
+            return a;
+        } else {
+            return ("0" + a);
+        }
+    }
+
+    setInterval(function() {
+        currentTime = Date.now();
+
+        currentAmount = startAmount + startAmount * interestInMonth /
+            millisecondsInMonth * (currentTime - startDate);
+        cashflow = currentAmount * interestInMonth;
+
+        capital = currentAmount;
+        capitalInteger = Math.floor(capital.toFixed(2));
+        capitalDecimal = DecimalExtractor(capital);
+        document.getElementById("capitalInteger").innerHTML = SpaceAdder(
+            capitalInteger
+        );
+        document.getElementById("capitalDecimal").innerHTML =
+            ". " + capitalDecimal + " &#8372;";
+
+        cashflowInteger = Math.floor(cashflow.toFixed(2));
+        cashflowDecimal = DecimalExtractor(cashflow);
+        document.getElementById("cashflowInteger").innerHTML = SpaceAdder(
+            cashflowInteger
+        );
+        document.getElementById("cashflowDecimal").innerHTML =
+            ". " + cashflowDecimal + " &#8372;";
+
+        goalCompletion = (parseInt(capital) * 100) / nextGoal;
+        goalCompletionInteger = Math.floor(goalCompletion.toFixed(2));
+        goalCompletionDecimal = DecimalExtractor(goalCompletion);
+        document.getElementById("completionInteger").innerHTML = SpaceAdder(
+            goalCompletionInteger
+        );
+        document.getElementById("completionDecimal").innerHTML =
+            ". " + goalCompletionDecimal + " %";
+
+        capitalGrow = Math.round(secondsInMonth / Math.floor(cashflow));
+        document.getElementById("grow").innerHTML =
+            SpaceAdder(Math.round(capitalGrow / 60));
+    }, 1000);
+}
