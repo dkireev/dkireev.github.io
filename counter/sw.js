291163a8d772c03a336f8f0f1b38e60e3cd854cc
Dima Kireiev <dkireev.com.ua@gmail.com>
2018-12-17 12:37:50 +0200

    Path fix

--- counter/sw.js
+++ counter/sw.js
@@ -2,20 +2,18 @@ var cacheName = 'CSv1';
 
 var cachedFiles = [
   '/',
-  'index.html',
-  'manifest.json',
-  'script.js',
-  'style.css',
-  'settings.png',
-  'edit.png',
-  'close.png'
+  '/index.html',
+  '/manifest.json',
+  '/script.js',
+  '/style.css',
+  '/settings.png',
+  '/edit.png',
+  '/close.png'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('Service Worker install event');
   event.waitUntil (
     caches.open(cacheName).then(function(cache) {
-      console.log('Caching files');
       return cache.addAll(cachedFiles);
     }).then(function() {
       return self.skipWaiting();
