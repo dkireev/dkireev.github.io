91c7fba0f676cc2452069acd95005e041b754f81
Dima Kireiev <dkireev.com.ua@gmail.com>
2018-12-17 14:33:24 +0200

    Updated Service worker

--- counter/sw.js
+++ counter/sw.js
@@ -1,24 +1,37 @@
-var cacheName = 'CSv1';
+var cacheName = 'CSv2';
 
 var cachedFiles = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'script.js',
-  'style.css',
-  'settings.png',
-  'edit.png',
-  'close.png'
+    '/',
+    'index.html',
+    'manifest.json',
+    'script.js',
+    'style.css',
+    'settings.png',
+    'edit.png',
+    'close.png'
 ];
 
 self.addEventListener('install', function(event) {
-  event.waitUntil (
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(cachedFiles);
-    }).then(function() {
-      return self.skipWaiting();
-    }).catch(function(error) {
-      console.log("Cache failed", error);
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(cachedFiles);
+        }).then(function() {
+            return self.skipWaiting();
+        }).catch(function(error) {
+            console.log("Cache failed", error);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
 });
