1f1cdf2d45e5d262c7e90610b7fe048384328cbc
Dima Kireiev <dkireev.com.ua@gmail.com>
2018-12-17 13:27:12 +0200

    Fix

--- counter/sw.js
+++ counter/sw.js
@@ -2,13 +2,13 @@ var cacheName = 'CSv1';
 
 var cachedFiles = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/script.js',
-  '/style.css',
-  '/settings.png',
-  '/edit.png',
-  '/close.png'
+  'index.html',
+  'manifest.json',
+  'script.js',
+  'style.css',
+  'settings.png',
+  'edit.png',
+  'close.png'
 ];
 
 self.addEventListener('install', function(event) {
