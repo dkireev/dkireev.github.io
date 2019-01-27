0f2c2d628faaece6fafbbfa021f9f7aec4bbcd8d
Dmitry <dkireev.com.ua@gmail.com>
2019-01-27 13:13:55 +0200

    Add

---
+++ deposit-vova/sw.js
@@ -0,0 +1,45 @@
+var cacheName = 'CSv2';
+
+var cachedFiles = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'script.js',
+    '../counter/style.css',
+    '../counter/settings.png',
+    '../counter/edit.png',
+    '../counter/close.png'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(cachedFiles);
+        }).then(function() {
+            return self.skipWaiting();
+        }).catch(function(error) {
+            console.log("Cache failed", error);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+    );
+});
