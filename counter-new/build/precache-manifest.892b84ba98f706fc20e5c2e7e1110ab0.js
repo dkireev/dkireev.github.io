34d8c6c4a9c25fb43b0e23c03b6a0af523384035
Dmitry <dkireev.com.ua@gmail.com>
2019-01-31 20:31:10 +0200

    moved

---
+++ counter-new/build/precache-manifest.892b84ba98f706fc20e5c2e7e1110ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "966fad9e312bba66b133",
+    "url": "/static/css/main.5a55dada.chunk.css"
+  },
+  {
+    "revision": "966fad9e312bba66b133",
+    "url": "/static/js/main.966fad9e.chunk.js"
+  },
+  {
+    "revision": "6f75dc1247dd508d597c",
+    "url": "/static/js/1.6f75dc12.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f47f3222ff3d82172d4ea09019102e4a",
+    "url": "/index.html"
+  }
+];
