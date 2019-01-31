1017d50d40a168cf056a931247cbbf80931fd1dc
Dmitry <dkireev.com.ua@gmail.com>
2019-01-31 20:33:57 +0200

    Build updated

---
+++ counter-new/build/precache-manifest.1c397fafae3e8a31a544b070d92ef356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b364a648cccfcbff6131",
+    "url": "/static/css/main.838e2bbf.chunk.css"
+  },
+  {
+    "revision": "b364a648cccfcbff6131",
+    "url": "/static/js/main.b364a648.chunk.js"
+  },
+  {
+    "revision": "34f035dcb935df6bc4d2",
+    "url": "/static/js/1.34f035dc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e61ef77ef4f16ad434565eefb2f80983",
+    "url": "/index.html"
+  }
+];
