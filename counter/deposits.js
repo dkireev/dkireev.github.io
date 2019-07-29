e9bfeb427d88ff07aa42c819e2f423fc39c719a2
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2019-07-29 11:29:48 +0300

    update deposit

--- counter/deposits.js
+++ counter/deposits.js
@@ -1,21 +1,22 @@
-const deposit = [{
-        "startDate": "Jul 1, 2019",
-        "startAmount": "69350.23",
-        "interestRate": "14.25"
-    },
-    {
-        "startDate": "Jul 8, 2019",
-        "startAmount": "114716.75",
-        "interestRate": "16.5"
-    },
-    {
-        "startDate": "Jul 22, 2019",
-        "startAmount": "23647.19",
-        "interestRate": "16.5"
-    },
-    {
-        "startDate": "Jul 8, 2019",
-        "startAmount": "20121.84",
-        "interestRate": "16.5"
-    }
+const deposit = [
+  {
+    startDate: "Jul 1, 2019",
+    startAmount: "69350.23",
+    interestRate: "14.25"
+  },
+  {
+    startDate: "Jul 8, 2019",
+    startAmount: "114716.75",
+    interestRate: "16.5"
+  },
+  {
+    startDate: "Jul 25, 2019",
+    startAmount: "23895.03",
+    interestRate: "16.5"
+  },
+  {
+    startDate: "Jul 8, 2019",
+    startAmount: "20121.84",
+    interestRate: "16.5"
+  }
 ];
