0174237f5825206d851a34247b04d939fdab48c8
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-03-10 20:04:48 +0200

    add build folder

---
+++ counter-new/build/precache-manifest.5d75ff23d8be2622672fd4049208ddf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd4b2b07e36f105c477871559806735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d7c18b06926b81c64a8",
+    "url": "/static/css/main.c898e1a1.chunk.css"
+  },
+  {
+    "revision": "a3eab6b5e0af3fd97b32",
+    "url": "/static/js/2.a2c35492.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a2c35492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7c18b06926b81c64a8",
+    "url": "/static/js/main.0d226792.chunk.js"
+  },
+  {
+    "revision": "1cf496623e1dfddcefaa",
+    "url": "/static/js/runtime-main.1962f475.js"
+  }
+]);
