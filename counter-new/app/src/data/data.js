89d0736d45aa24402cc712347a7557eedc7e5148
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-04-20 10:54:48 +0300

    update assets

--- counter-new/app/src/data/data.js
+++ counter-new/app/src/data/data.js
@@ -1,60 +1,60 @@
 export const goal = {
   currency: "USD",
-  amount: 1000000
+  amount: 1000000,
 };
 
 export const assets = {
   crypto: [
     {
       currency: "BTC",
-      amount: 0.0240864
-    }
+      amount: 0.02447652,
+    },
   ],
   securities: [
     {
       currency: "EUR",
-      amount: 334.32
+      amount: 390.38,
     },
     {
       currency: "EUR",
-      amount: 124.92
-    }
+      amount: 254.3,
+    },
   ],
   deposits: [
     {
       start: "Apr 4, 2020",
       currency: "UAH",
-      amount: 97598.40,
+      amount: 97598.4,
       interest: 14.2,
-      tax: 19.5
+      tax: 19.5,
     },
     {
-      start: "Mar 10, 2020",
+      start: "Apr 10, 2020",
       currency: "UAH",
-      amount: 174817.6,
+      amount: 176843.24,
       interest: 17,
-      tax: 19.5
+      tax: 19.5,
     },
     {
-      start: "Mar 11, 2020",
+      start: "Apr 11, 2020",
       currency: "UAH",
-      amount: 32263.59,
+      amount: 32637.43,
       interest: 17,
-      tax: 19.5
+      tax: 19.5,
     },
     {
       start: "Apr 3, 2020",
       currency: "UAH",
       amount: 30572.65,
       interest: 16,
-      tax: 19.5
+      tax: 19.5,
     },
     {
       start: "Mar 21, 2020",
       currency: "UAH",
       amount: 10209.59,
       interest: 13.75,
-      tax: 19.5
-    }
-  ]
+      tax: 19.5,
+    },
+  ],
 };
