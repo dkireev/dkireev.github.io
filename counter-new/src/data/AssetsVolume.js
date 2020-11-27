a613bba988399abfb38eebb9f9ad9d9fac805e54
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-11-27 20:39:33 +0200

    add some git ignores

--- counter-new/src/data/AssetsVolume.js
+++
@@ -1,7 +0,0 @@
-const assetsVolume = (asset, rates) => {
-  return asset.reduce((acc, item) => {
-    return acc + (1 / rates[item.currency]) * item.amount;
-  }, 0);
-};
-
-export default assetsVolume;
