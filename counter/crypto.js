bb5838d682092dca21b6f5b933c8131b5764a676
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2021-09-21 10:09:28 +0300

    remove redundant staff

--- counter/crypto.js
+++
@@ -1,6 +0,0 @@
-const crypto = [
-  {
-    currency: "BTC",
-    amount: 0.02127568
-  }
-];
--- deposit-vova/deposits.js
+++
@@ -1,7 +0,0 @@
-const deposit = [
-  {
-    startDate: "Feb 27, 2019",
-    startAmount: "101154.59",
-    interestRate: "15"
-  }
-];
